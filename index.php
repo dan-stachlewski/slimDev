11594c8819b9051afe1b86dafe69523458ebc1c9
dan-stachlewski <dan.stachlewski@gmail.com>
2015-10-06 12:43:33 +1100

    Introduced to $app->get() method and the args $request, $response, $args that route the user through the ROOT index.php file. It creates a neater way to display URL's and can be used to navigate to a particular page of a web site if you know the URL. ***NOTE*** Need to follow up on the get, post, etc methods and their responses - need to read up on slim.

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
 <?php
+require './vendor/autoload.php';
+//START OF APP
+$app = new Slim\App();
+
+//All APP data wuthin these lines of code...
+
+//This says route the index page start @ root = / then use function to get you going
+//$request - what your asking for get post cookies
+//$response - what you are getting back
+//read from request get from response
+//$args - 
+//function is anonymous
+$app->get('/', function($request, $response, $args){
+    $response->write("Welcome to SLIM!");
+    return $response;
+    
+});
+//{$args['name']} will come from the URL
+$app->get('/hello/{name}', function($request, $response, $args){
+    $msg = "Hello {$args['name']} - in SLIM Style!!!";
+    $response->write($msg);
+    return $response;
+});
+//API for restful web service
+//routers map urls to 
+//templates prep for data ref 28.22min
+//get = http verb that is used
+//post
+//
+//when asking for url u r generating a get request
+//programming interface to http protocol
+//
+$app->get('/apples.html', 'get_apples');
+function get_apples($request, $response, $args) {
+    $response->write("in Apples.html");
+    return $response;
+}
+
+$app->run();
+//END OF APP
--- readMe.php
+++ readMe.php
@@ -13,11 +13,25 @@
  * STEP [10] ALWAYS START index.php file w:
  * - require './vendor/autoload.php';
  * - as it Autoloads all the libraries maintained by composer
-  * STEP [11] 
- * * STEP [12] 
- * STEP [13] 
- * STEP [14] 
- * STEP [15] 
+  * STEP [11] Every app has a START LINE of code: $app = new Slim\App();
+ * * STEP [12] Every app has a END LINE of code: $app->run();
+ * STEP [13] All other code is written b/w those 2 lines of code.
+ * STEP [14] Example 1: $app->get('/', function($request, $response, $args){}
+ * - These examples deal w routing the index.php page everything goes thru this page
+ * - This says route the index page start @ root = / then use function to get you going
+ * $request - what your asking for get post cookies
+ * $response - what you are getting back
+ * read from request get from response
+ * $args - 
+ * function is anonymous
+ * STEP [15]
+ * API for restful web service
+ * routers map urls to 
+ * templates prep for data ref 28.22min
+ * get = http verb that is used
+ * post
+ * when asking for url u r generating a get request
+ * programming interface to http protocol
  * STEP [16] 
  * STEP [17] 
  * SETP [18] 
