1906df054af15ee815cb6f9b2e269b337bc71d95
dan-stachlewski <dan.stachlewski@gmail.com>
2015-10-06 12:33:18 +1100

    Created a new Project SlimHello Installed all Dependencies via Composer Using Version 3.0.0 RC1 Tried to get around issue w pimple lib by adding .htaccess file from teacher but didn't work b/c its the RC1 version. Set up the /Index.php file and a readMe.php w instructions on what to do.

---
+++ readMe.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * STEP [1] Create NEW PROJECT
+ * STEP [2] RIGHT CLICK NEW PROJECT
+ * STEP [3] Choose COMPOSER -> ADD DEPENDENCY
+ * STEP [4] Search for SLIM
+ * STEP [5] Use VERSION 3.0.0 RC1 (latest version - not yet approved)
+ * STEP [6] Click REQUIRE
+ * STEP [7] Add .htaccess file (supplied by teacher)
+ * STEP [8] Set Up Versioning w GitHub and Committed files
+ * STEP [9] Set up ReadMe file
+ * STEP [10] ALWAYS START index.php file w:
+ * - require './vendor/autoload.php';
+ * - as it Autoloads all the libraries maintained by composer
+  * STEP [11] 
+ * * STEP [12] 
+ * STEP [13] 
+ * STEP [14] 
+ * STEP [15] 
+ * STEP [16] 
+ * STEP [17] 
+ * SETP [18] 
+ * STEP [19] 
+ */
+
